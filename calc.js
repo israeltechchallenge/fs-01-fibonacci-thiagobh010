6cec0c19506a55a0092625bf3b4c10d1e5a088d0
Thiago <107802050+thiagobh010@users.noreply.github.com>
2022-07-02 23:22:41 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,8 +1,7 @@
-spinnerHide();
-let x = 0;
-let y = 0;
 
-/*function fiboN(n) {
+spinnerHide();
+
+function fiboN(n) {
   let a = 0, b = 1, c, i;
   if (n == 0)
     return a;
@@ -12,41 +11,33 @@ let y = 0;
     b = c;
   }
   return b;
-} */
-
-function spinnerShow(){
-  document.getElementById('fiboResp').innerHTML = '';
-  document.getElementById('hideSpin').style.visibility = 'visible';
-  setTimeout(() => {
-    fiboCalc();
-  }, 500);
 }
 
-function fiboCalc() {  
-  x = parseInt(document.getElementById('inputN').value);
-  if (x == 42) {
-    spinnerHide();
-    document.getElementById('fiboResp').innerHTML = "42 is the meaning of life";
-    return document.getElementById('fiboResp').style.color = '#FF0000';
+function validateToCalc() {
+  let x = parseInt(document.getElementById('inputN').value);
+  document.getElementById('fiboResp').innerHTML = '';
+  document.getElementById('hideSpin').style.visibility = 'visible';
+  let saveCheckDefault = document.querySelector("#flexCheckDefault");
+  if (x && saveCheckDefault.checked) {
+    return callServer(x);
   }
   if (x > 50) {
     spinnerHide();
-    return document.getElementById('fiboResp').innerHTML = `<div class="alert alert-danger" role="alert">
-    Can't be larger than 50</div>`;
+    return document.getElementById('fiboResp').innerHTML = `<div class="alert alert-danger" role="alert">Can't be larger than 50</div>`;
   }
   if (x < 1) {
     spinnerHide();
+    document.getElementById('fiboResp').style.color = '#373A3C';
     return document.getElementById('fiboResp').innerHTML = "Number can't be smaller than 1";
-  } 
-  callServer(x);    // y = fiboN(x);
-  spinnerHide();
-  document.getElementById('fiboResp').innerHTML = y;
-};
+  }
+  fiboCalc(x);
+}
 
-function spinnerHide() {
-  setTimeout(() => {
-    document.getElementById('hideSpin').style.visibility = 'hidden';
-  }, 100);
+function fiboCalc(x) {
+  let y = fiboN(x);
+  spinnerHide();
+  document.getElementById('fiboResp').style.color = '#373A3C';
+  document.getElementById('fiboResp').innerHTML = y;
 };
 
 function callServer(x) {
@@ -54,20 +45,66 @@ function callServer(x) {
     .then(response => {
       console.log('response: ', response);
       if (response.ok) {
-        response.json()
+        return response.json()
           .then(data => {
             spinnerHide();
             console.log('Resp: ', data.result);
             document.getElementById('fiboResp').style.color = '#373A3C';
             document.getElementById('fiboResp').innerHTML = data.result;
+            document.getElementById('listResults').innerHTML = '';
+            getResults();
           })
-          .catch((err)=>{
+      } else {
+        console.log('Entrou no ELSE: ');
+        return response.text()
+        .then((data) => {
+          console.log('Then fetch: ', data);
+          spinnerHide();
+          document.getElementById('fiboResp').style.color = '#FF0000';
+          document.getElementById('fiboResp').innerHTML = data;
+          document.getElementById('listResults').innerHTML = '';
+          getResults();
+        })
+      }
+    });
+}
+
+function getResults() {
+  let resp_list = [];
+  fetch(`http://localhost:5050/getFibonacciResults`)
+    .then(response => {
+      if (response.ok) {
+        response.json()
+          .then(data => {
+            spinnerHide();
+            console.log('Resp: ', data.results);
+            const itemList = document.getElementById('listResults');
+            resp_list = data.results;
+            resp_list.forEach(item => {
+              let date = new Date(item.createdDate);
+              let li = document.createElement('p');
+              let txt = `The Fibonnaci Of ${item.number} is ${item.result}. Calculated at: ${date.toString('IL')}`;
+              let node = document.createTextNode(txt);
+              li.appendChild(node);
+              itemList.appendChild(li);
+            })
+          })
+          .catch((err) => {
             console.log('Err Resp: ', err);
           });
-      }else {
-        spinnerHide();
-        document.getElementById('fiboResp').innerHTML = "42 is the meaning of life";
-        return document.getElementById('fiboResp').style.color = '#FF0000';
+      } else {
+        throw new Error(response.statusText);
       }
-    }) 
-} 
+    });
+}
+
+function spinnerHide() {
+  setTimeout(() => {
+    document.getElementById('hideSpin').style.visibility = 'hidden';
+  }, 100);
+};
+
+function load(){
+  document.getElementById('calcBtn').addEventListener('click', validateToCalc);
+  getResults();
+}
